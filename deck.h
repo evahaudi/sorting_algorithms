c60e05b9209e158185ca422a11d6ea267968a4fe
everlyne akinyi <evahaudi2354@gmail.com>
2023-09-27 08:37:19 +0000

    updated the header files

--- deck.h
+++ deck.h
@@ -11,12 +11,12 @@
  * @DIAMOND: Diamonds suit.
  */
 typedef enum kind_e
-{
-	SPADE = 0,
-	HEART,
-	CLUB,
-	DIAMOND
-} kind_t;
+  {
+   SPADE = 0,
+   HEART,
+   CLUB,
+   DIAMOND
+  } kind_t;
 
 /**
  * struct card_s - Playing card
@@ -27,8 +27,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-	const char *value;
-	const kind_t kind;
+  const char *value;
+  const kind_t kind;
 } card_t;
 
 /**
@@ -40,11 +40,14 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
+  const card_t *card;
+  struct deck_node_s *prev;
+  struct deck_node_s *next;
 } deck_node_t;
 
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
 void sort_deck(deck_node_t **deck);
-
 #endif /* DECK_H */
