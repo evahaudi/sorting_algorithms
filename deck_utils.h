72d72f3add756676cbe6f94180c2314a10c36376
everlyne akinyi <evahaudi2354@gmail.com>
2023-09-27 08:49:13 +0000

    updated utils

---
+++ deck_utils.h
@@ -0,0 +1,10 @@
+#ifndef DECK_UTILS_H
+#define DECK_UTILS_H
+
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_UTILS_H */
---
+++ sort_utils.h
@@ -0,0 +1,30 @@
+#ifndef SORT_UTILS_H
+#define SORT_UTILS_H
+void swap_ints(int *a, int *b);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+void quick_sort_hoare(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		  size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_UTILS_H */
